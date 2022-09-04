97fe3566c2ed1563e5d29378a191959d5f2bd01d
waavie-admin <harish@waavie.com>
2022-09-04 16:54:40 +0530

    frontend added

--- cms-backend/config/plugins.js
+++ cms-backend/config/plugins.js
@@ -18,6 +18,18 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: false,
+        depthLimit: 7,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+      },
+    },
     // ...
   });
   
